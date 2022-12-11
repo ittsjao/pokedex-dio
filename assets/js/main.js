705052eacb2ab94cd7643ad084e868db4896a278
unknown <jaocesarsantos@gmail.com>
2022-12-11 19:25:25 -0300

    Pokedex

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+const pokemonList = document.getElementsByClassName('pokemonList')
+const loadMoreButton = documento.getElementsById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 10
+let offset = 0
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+                <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class"name">${pokemon.name}</span>
+        
+                    <div class"detail"> 
+                        <ol class="types"]
+                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                        </ol>
+        
+                        <img src="${pokemon.photo}"
+                        alt="${pokemon.name}">
+                    </div>
+                </li>
+            `
+            ).join('')
+
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordNextPageWhitNexPage = offset + limit
+
+    if (qtdRecordNextPageWhitNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadMoreButton(offset, newLimit) 
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
