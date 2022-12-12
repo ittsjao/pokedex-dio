a84397cc3e22e6dc4edba3f3eed8d4e6c4381784
João César dos Santos Quirino <jaocesarsantos@gmail.com>
2022-12-11 21:09:13 -0300

    Reparos gerais, codigo todo funcionando

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -3,16 +3,16 @@ const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
-    pokemon.number = pokemonDetail.id
-    pokemon.name = pokemonDetail.name
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
 
-    const types = pokemonDetail.types.map((typeSlot) => typeSlot.type.name)
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
     pokemon.types = types
     pokemon.type = type
 
-    pokemon.photo = pokemon.sprites.other.dream_world.front_default
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
     return pokemon
 }
@@ -23,7 +23,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
